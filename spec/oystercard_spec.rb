1dfa014bab947e8b5045fd9c4671647a4b93c429
Harry Parsons <hparsons93@gmail.com>
2021-08-31 14:22:32 +0100

    two tests

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,8 +1,16 @@
 require './lib/oystercard.rb'
 
 describe Oystercard do 
+  
   it 'should respond to #balance' do 
   oyster = Oystercard.new 
   expect(oyster).to respond_to(:balance)
 end 
+
+  it 'has an initial balance of 0' do 
+    oyster = Oystercard.new
+    expect(oyster.balance).to eq(0)
+end 
+ 
+
 end 
