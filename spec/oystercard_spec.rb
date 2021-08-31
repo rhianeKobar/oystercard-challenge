161cca64c9636ef518e090f6332d4f55c90c5f25
Harry Parsons <hparsons93@gmail.com>
2021-08-31 14:31:20 +0100

    added top_up method

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -5,12 +5,18 @@ describe Oystercard do
   it 'should respond to #balance' do 
   oyster = Oystercard.new 
   expect(oyster).to respond_to(:balance)
-end 
+  end 
 
   it 'has an initial balance of 0' do 
     oyster = Oystercard.new
     expect(oyster.balance).to eq(0)
-end 
+  end 
+
+  it 'should respond to #top_up' do 
+  oyster = Oystercard.new 
+  expect(oyster).to respond_to(:top_up)
+  end 
+
  
 
 end 
